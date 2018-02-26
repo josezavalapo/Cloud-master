eef526b11c96b5810478faaaf430d408bbd3f981
José Alonso <jose.zavalapo@udlap.mx>
2018-02-26 14:18:42 -0600

    agregar componentes
    se agregaron los componentes de login y se cambiaron nombres

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './login.html',
+  styleUrls: ['./login-style.css']
 })
 export class AppComponent {
   title = 'app';
