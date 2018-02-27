b179093e3154f28f89fa7a4bf770924b1ab787b5
José Alonso <jose.zavalapo@udlap.mx>
2018-02-27 13:20:42 -0600

    actualizacion con back-end
    agregar elementos para conectar al back-end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './login.html',
-  styleUrls: ['./login-style.css']
+  templateUrl: './main.html',
+  styleUrls: ['./main-style.css']
 })
 export class AppComponent {
   title = 'app';
